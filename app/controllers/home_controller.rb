7363a640d65cf9807daac25f0d45fbed782c6ad9
ckyleflynn <ckflynn@csustudent.net>
2020-02-29 17:30:14 -0500

    input stock

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 class HomeController < ApplicationController
   def index
     @api =StockQuote::Stock.new(api_key: 'pk_cf8703fa79304007b7352d1e18ddae15')
-    if params[:ticker]
+    if params[:ticker] == ""
+      @nothing = "Enter Ticker (Stock) Symbol"
+    elsif params[:ticker]
       @stock = StockQuote::Stock.quote(params[:ticker])
+  ## can't get the error to show. unknown symbol??
+  ##      if !@stock
+  ##         @error = "That is not a valid ticker symbol!"
+  ##      end
+
 	   end
 
   end
