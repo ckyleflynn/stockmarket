6efe5cd848f8d60bc09419418a61a6b30414e282
ckyleflynn <ckflynn@csustudent.net>
2020-02-29 15:05:06 -0500

    stock

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap', '~> 4.4', '>= 4.4.1'
 gem 'jquery-rails'
+gem "stock_quote", '~> 3.0.0'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @api =StockQuote::Stock.new(api_key: 'pk_cf8703fa79304007b7352d1e18ddae15')
+    if params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+	   end
+
   end
 
   def about
